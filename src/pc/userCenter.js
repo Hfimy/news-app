a4fb11fc228d7c1b7b5dc5e28a8f0cc34406e1ba
Hfimy <780986227@qq.com>
2017-12-13 23:47:04 +0800

    添加用户头像

--- src/pc/userCenter.js
+++ src/pc/userCenter.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import { Row, Col, Tabs, Card, message } from 'antd'
+import { Row, Col, Tabs, Card, message,Avatar,Upload,Icon } from 'antd'
 const TabPane = Tabs.TabPane
 
 export default class UserCenter extends PureComponent {
@@ -14,6 +14,7 @@ export default class UserCenter extends PureComponent {
     state = {
         comments: [],
         collections: [],
+        loading:false
     }
 
     componentWillMount() {
@@ -40,9 +41,21 @@ export default class UserCenter extends PureComponent {
     componentWillUnmount() {
         this._isMounted = false;
     }
+    handleChange=(info)=>{
+        // if(info.file.status==='uploading'){
+        //     this.setState({loading:true});
+        //     return;
+        // }
+        // if(info.file.status==='done'){
 
+        // }
+        console.log('onChange')
+    }
+    beforeUpload=()=>{
+        console.log('before')
+    }
     render() {
-        const { comments, collections } = this.state;
+        const { comments, collections ,imageUrl,loading} = this.state;
         const commentList = comments.length
             ? comments.map((item, index) => (
                 <Card key={index}>
@@ -58,7 +71,14 @@ export default class UserCenter extends PureComponent {
                 </Card>
             ))
             : '您还没有收藏任何的新闻，快去收藏吧~'
+        const uploadButton=(
+            <div>
+                <Icon type={loading?'loading':'plus'}/>
+                <div class='ant-upload-text'>Upload</div>
+            </div>
+        )
         return (
+
             <Row>
                 <Col span={6} />
                 <Col span={14}>
@@ -68,6 +88,19 @@ export default class UserCenter extends PureComponent {
                         </TabPane>
                         <TabPane key='2' tab='我的收藏列表'>
                             {collectionList}
+                        </TabPane>
+                        <TabPane key='3' tab='我的个人资料'>
+                            <Avatar size='large' icon='user' />
+                            <Upload
+                                name='avatar'
+                                listType='picture-card'
+                                class='avatar-uploader'
+                                showUploadList={false}
+                                action=''
+                                beforeUpload={this.beforeUpload}
+                                onChange={this.handleChange}>
+                                {imageUrl?<img src={imageUrl} alt=''/>:uploadButton}
+                            </Upload>
                         </TabPane>
                     </Tabs>
                 </Col>
